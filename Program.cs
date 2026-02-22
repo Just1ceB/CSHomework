d65b52823b4cfa34b6809df3ab80c3ae410760f9
just1ceb <chpshonok@gmail.com>
2026-02-23 01:29:41 +0200

    Clearing the master branch so it will be nothing but the functions

--- Program.cs
+++ Program.cs
@@ -7,48 +7,7 @@ namespace Prjcts
     {
         static void Main(string[] args)
         {
-            //ClassPractice.UnitTest();
-            //VolleyBallPlayer.UnitTest();
-
-            bool repeatChoosing = false;
-
-            do
-            {
-                Console.Write("1 - Point\n2 - Dates\n3 - Rectangle\n4 - Rational\n5 - Passport\n0 - Close\nChoose: ");
-
-                switch (Console.ReadKey().KeyChar)
-                {
-                    case '0':
-                        Console.WriteLine();
-                        Console.Write("Closed");
-                        break;
-                    case '1':
-                        Console.WriteLine();
-                        Point.UnitTest();
-                        break;
-                    case '2':
-                        Console.WriteLine();
-                        Date.UnitTest();
-                        break;
-                    case '3':
-                        Console.WriteLine();
-                        Rectangle.UnitTest();
-                        break;
-                    case '4':
-                        Console.WriteLine();
-                        Rational.UnitTest();
-                        break;
-                    case '5':
-                        Console.WriteLine();
-                        Passport.UnitTest();
-                        break;
-                    default:
-                        Console.WriteLine();
-                        Console.WriteLine("Error");
-                        repeatChoosing = Funcs.RepeatChoosing();
-                        break;
-                }
-            } while (repeatChoosing);
+            // Master branch is clear because I don't know what to do with it so I will leave it like this carelessly, only with cool functions I made (except that one with word finder)
         }
     }
 }
